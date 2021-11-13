da784c92081699fdf09b3d40c5268144039573e9
Per Grapatin <per.grapatin@gmail.com>
2021-11-13 10:18:36 +0100

    Template day linted

--- src/dayTemplate/day.py
+++ src/dayTemplate/day.py
@@ -1,30 +1,36 @@
-#import cmath for complex number operations
-from abc import abstractproperty
-import cmath
+"""
+Template code
+"""#import cmath for complex number operations
+#from abc import abstractproperty
+#import cmath
 #import Path for file operations
 from pathlib import Path
 
-problemInputTxt = Path("/Users/pergrapatin/Source/AOC2017/src/dayXX/input.txt").read_text()
+PROGBLEM_INPUT_TXT = Path("/Users/pergrapatin/Source/AOC2017/src/"\
+    +"dayXX/input.txt").read_text()
 
-exampleInput1 = """aaaaa-bbb-z-y-x-123[abxyz]
+EXAMPLE_INPUT1 = """aaaaa-bbb-z-y-x-123[abxyz]
 a-b-c-d-e-f-g-h-987[abcde]
 not-a-real-room-404[oarel]
 totally-real-room-200[decoy]"""
-exampleResult1 = 1514
+EXAMPLE_RESULT1 = 1514
 
-def stringWorker(input):
-    aSteps = input.split("\n")
-    return aSteps
+def string_worker(input_string):
+    """Helper string worker function
+    """
+    a_steps = input_string.split("\n")
+    return a_steps
 
-def problemA(input, expectedResult):
-    solution = -1
+def problem_a(input_string, expected_result):
+    """Problem A solved function
+    """
+    solution = input_string
 
-    if solution == expectedResult:
+    if solution == expected_result:
         print("Correct solution found:", solution)
     else:
-        print("Incorrect solution, we got:", solution, "expected:", expectedResult)
+        print("Incorrect solution, we got:", solution, "expected:", expected_result)
 
-problemA(exampleInput1, exampleResult1)
-problemA(problemInputTxt, 0)
+problem_a(EXAMPLE_INPUT1, EXAMPLE_RESULT1)
+problem_a(PROGBLEM_INPUT_TXT, 0)
 print("\n")
-
